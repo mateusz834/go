51026a74902da34dc32ba5e7e086126b5367ef9e
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2024-09-29 19:01:01 +0200

    update
    Change-Id: I94038eb28740530d56595a40602bd3519c281c2b

--- src/go/format/benchmark_test.go
+++ src/go/format/benchmark_test.go
@@ -57,7 +57,7 @@ var tests = []struct {
 	// add new test cases here as needed
 }
 
-func BenchmarkSource(b *testing.B) {
+func BenchmarkFormat(b *testing.B) {
 	var src bytes.Buffer
 	for _, t := range tests {
 		src.Reset()
